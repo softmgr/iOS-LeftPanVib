4654f52fce057a527b31116103307002d42cfe35
softmgr <34020339+softmgr@users.noreply.github.com>
2026-09-19 19:36:29 +0800

    Add haptic feedback to UINavigationController pop gesture
    Refactor UINavigationController to add haptic feedback on interactive pop gestures. Implement method swizzling for viewDidLoad to ensure proper target assignment for gesture recognizer.

--- leftPan_vib.m
+++ leftPan_vib.m
@@ -1,30 +1,39 @@
 #import <UIKit/UIKit.h>
 #import <objc/runtime.h>
 
-@implementation UINavigationController (VibePop)
-
-+ (void)load {
-    static dispatch_once_t onceToken;
-    dispatch_once(&onceToken, ^{
-        Class cls = [self class];
-        Method orig = class_getInstanceMethod(cls, @selector(viewDidLoad));
-        Method swiz = class_getInstanceMethod(cls, @selector(vibe_viewDidLoad));
-        method_exchangeImplementations(orig, swiz);
-    });
-}
-
-- (void)vibe_viewDidLoad {
-    [self vibe_viewDidLoad];
-    if (self.interactivePopGestureRecognizer) {
-        [self.interactivePopGestureRecognizer addTarget:self action:@selector(vibe_handlePop:)];
-    }
-}
-
-- (void)vibe_handlePop:(UIGestureRecognizer *)gesture {
+static void vibe_handlePop(id self, SEL _cmd, UIGestureRecognizer *gesture) {
     if (gesture.state == UIGestureRecognizerStateBegan) {
-        UIImpactFeedbackGenerator *feedback = [[UIImpactFeedbackGenerator alloc] initWithStyle:UIImpactFeedbackStyleMedium];
-        [feedback impactOccurred];
+        dispatch_async(dispatch_get_main_queue(), ^{
+            UIImpactFeedbackGenerator *generator = [[UIImpactFeedbackGenerator alloc] initWithStyle:UIImpactFeedbackStyleMedium];
+            [generator prepare];
+            [generator impactOccurred];
+        });
     }
 }
 
-@end
+static void (*orig_viewDidLoad)(id, SEL);
+
+static void swiz_viewDidLoad(UIViewController *self, SEL _cmd) {
+    orig_viewDidLoad(self, _cmd);
+    
+    if ([self isKindOfClass:[UINavigationController class]]) {
+        UINavigationController *nav = (UINavigationController *)self;
+        if (nav.interactivePopGestureRecognizer) {
+            // 避免重复添加 target 导致单次手势震动多次
+            [nav.interactivePopGestureRecognizer removeTarget:nav action:@selector(vibe_handlePop:)];
+            [nav.interactivePopGestureRecognizer addTarget:nav action:@selector(vibe_handlePop:)];
+        }
+    }
+}
+
+__attribute__((constructor)) static void init_vib(void) {
+    Class navClass = [UINavigationController class];
+    
+    class_addMethod(navClass, @selector(vibe_handlePop:), (IMP)vibe_handlePop, "v@:@");
+    
+    Method origMethod = class_getInstanceMethod(navClass, @selector(viewDidLoad));
+    if (origMethod) {
+        orig_viewDidLoad = (void (*)(id, SEL))method_getImplementation(origMethod);
+        method_setImplementation(origMethod, (IMP)swiz_viewDidLoad);
+    }
+}
